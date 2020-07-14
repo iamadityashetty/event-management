ca75efa1ff9ea381c980fe878d1fee1063fc6f1f
adityashetty <adishetty1234@gmail.com>
2020-07-14 23:19:03 +0530

    added apis for updating event status, getting events created by user and login

--- server/routes/api/events.js
+++ server/routes/api/events.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db-init/dbConn");
-// const validateUser = require("../../db-init/validation").validateUser;
+const validateUser = require("../../db-init/validation").validateUser;
 // const sendOTP = require("../../middlewares/sendOTP").sendOTP;
-// const bcrypt = require("bcrypt");
-// const generateToken = require("../../middlewares/token").generateToken;
+const bcrypt = require("bcrypt");
+const generateToken = require("../../middlewares/token").generateToken;
 
 /*
     @route to get all events
     @required id (event id) 
+    @required event_status (event status)
 */
 router.get("/:id", async (req, res, next) => {
   try {
-    const id = req.params.id;
-    let allEvents = await db.any(`select * from events where id = ${id}`);
+    const { id } = req.params;
+    const { event_status } = req.body;
+    let allEvents = await db.any(
+      `select * from events where id = ${id} AND event_status ='${event_status}'`
+    );
     if (allEvents.length === 0) {
       // sends response code of 400 with an error message
       res.status(400).json({
@@ -26,6 +30,33 @@ router.get("/:id", async (req, res, next) => {
         status: 200,
         message: "All events retrieved successfully",
         data: allEvents[0],
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/*
+    @route to get all events created by user
+    @required id (event id) 
+*/
+router.get("/user/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    let userEvents = await db.any(`select * from events where user_id = ${id}`);
+    if (userEvents.length === 0) {
+      // sends response code of 400 with an error message
+      res.status(400).json({
+        status: 400,
+        message: "No event found",
+      });
+    } else {
+      // sends a 200 status code with a successs message
+      res.status(200).json({
+        status: 200,
+        message: "All events retrieved successfully",
+        data: userEvents[0],
       });
     }
   } catch (error) {
@@ -133,7 +164,7 @@ router.delete("/:id", async (req, res, next) => {
   try {
     const id = req.params.id;
     const events = await db.any(
-      `delete from events where id=${id} returning id`
+      `delete from events where id = ${id} returning id`
     );
     if (events.length !== 1) {
       throw {
@@ -146,6 +177,119 @@ router.delete("/:id", async (req, res, next) => {
       message: "Deleted one event",
       data: events,
     });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+/*
+    @route to update an event status
+    @required id (event id)
+    @required event_status (event status)
+*/
+router.put("/status/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { event_status } = req.body;
+
+    if (!id) {
+      //Check if valid id is provided.
+      //If not, throw an error with appropriate message
+      throw {
+        statusCode: 400,
+        customMessage: "Event id is required",
+      };
+    }
+    //Create initial query
+
+    const eventStatus = `update users set event_status = '${event_status}' where id = ${id}`;
+    await db.any(eventStatus);
+    //Run the query
+
+    //Send response to user along with all updated fields
+    res.status(200).json({
+      status: 200,
+      message: "Updated Event Status Successfully",
+      data: eventStatus,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+/*
+    @route to login
+    @required email (email address)
+    @required password (password)
+*/
+router.post("/login", async (req, res, next) => {
+  try {
+    // checks if email and password params are passed or not, if not throws error
+    if (req.body) {
+      if (
+        !req.body.hasOwnProperty("email") ||
+        !req.body.hasOwnProperty("password")
+      ) {
+        throw {
+          statusCode: 400,
+          customMessage: "params are absent",
+        };
+      }
+    }
+
+    const { email, password } = req.body;
+
+    const user = {
+      email,
+    };
+
+    // checks for the validation of email
+    const { error } = validateUser(user);
+
+    // throws the error message if email param is empty or email is invalid
+    if (error !== undefined) {
+      console.log("ValidationError: ", error.details[0].message);
+      throw {
+        statusCode: 404,
+        customMessage: error.details[0].message.replace(/"/g, ""),
+      };
+    }
+
+    // checks if the email id already exists or not
+    let query = `select * from users where email = '${email}'`;
+    let userResult = await db.any(query);
+
+    // returns 400 status with message user already exists
+    if (userResult.length < 1) {
+      throw {
+        statusCode: 400,
+        customMessage: "Account not found",
+      };
+    } else {
+      // checking the user entered password with existing db password
+      if (!bcrypt.compareSync(password, userResult[0].password)) {
+        throw {
+          statusCode: 404,
+          customMessage: "Invalid credentials",
+        };
+      }
+
+      // sets the token with payload id and role (user or admin)
+      const token = generateToken({
+        user_id: userResult[0].id,
+        role: userResult[0].role,
+      });
+
+      // sends positive response to client with token, userData, message and status code 200
+      res.status(200).json({
+        statusCode: 200,
+        token: token,
+        userData,
+        message: "Logged in successful",
+      });
+    }
   } catch (err) {
     console.log(err);
     next(err);
